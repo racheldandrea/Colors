9906ee3ca0da7d4100736fb1ec9483488dcaedc0
racheldandrea <rachbeth03@gmail.com>
2025-03-03 13:20:08 -0600

    Add files via upload

---
+++ Colors/Colors/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by D'Andrea, Rachel E on 3/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var navView: UIView!
+    
+    
+    @IBAction func clickMenu(_ sender: UIBarButtonItem) {
+        
+        
+        
+        if(navView.isHidden) {
+                 navView.isHidden = false
+             }
+             else {
+                 navView.isHidden = true
+             }
+        
+        
+    }
+    
+
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navView.isHidden = true
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
